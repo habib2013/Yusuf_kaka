a0195b5389f04eb27674fff5074722a79baedb2a
habib2013 <nairabet25@gmail.com>
2020-05-18 01:03:47 +0100

    --- Auto-Git Commit ---

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -35,26 +35,14 @@ public function create_events(Request $request){
             'title' => 'required',
             'description' => 'required',
             'images' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-            'video_link' => 'url',
+            'video_link' => 'required|url',
             'date' =>'required',
             'address' => 'required'
         ]);
 
-        if($validator->passes()){
-            $input = $request->all();
-            $input['slug'] = time().rand(10,1000);
-            $input['images'] = time().'.'.$request->images->extension();
-            $request->images->move(public_path('images'),$input['images']);
+        $input = $request->all();
+               
 
-            
-            Event::create($input);
-
-            //session()->set('success','Post created successfully.');
-            return response()->json(['success'=>'done']);
-                        }
-                        else{
-                            return response()->json(['error'=>$validator->errors()->all()]);
-                        }
 
     }
 }
